a03edf2e411bfb153e1658a326971330f2473eed
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-11-11 13:11:34 -0800

    MongoDB setup

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
 const express = require('express');
-const expressLayout = require('express-ejs-layouts')
+const expressLayout = require('express-ejs-layouts');
+
+const connectDB = require('./server/config/db');
+connectDB();
 
 const app = express();
 const PORT = 5000 || process.env.PORT;
